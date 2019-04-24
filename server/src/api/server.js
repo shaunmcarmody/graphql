79363f1f5813de7a3d8c2684a100b6dd9d8c8b4e
shaunmcarmody <shaun.carmody@live.co.uk>
2019-04-24 17:54:24 +0100

    will bypass auth for Signup & LoginUser Mutations

--- server/src/api/server.js
+++ server/src/api/server.js
@@ -1,4 +1,4 @@
-const { ApolloServer, AuthorizationError } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema.js');
 const resolvers = require('./resolvers.js');
 const restricted = require('../utils/restricted.js');
@@ -6,10 +6,7 @@ const restricted = require('../utils/restricted.js');
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => restricted(req).then(res => res).catch(err => {
-    console.log('Server.js Error:', err.message);
-    throw new AuthorizationError('you must be logged in'); 
-  })
+  context: ({ req }) => restricted(req)
 });
 
 module.exports = server;
--- server/src/utils/restricted.js
+++ server/src/utils/restricted.js
@@ -3,15 +3,16 @@ const jwt = require('jsonwebtoken');
 
 const secret = 'keep it secret, keep it safe'; // replace with .env
 
-module.exports = async ({ headers, body }) => {
+module.exports = ({ headers, body }) => {
   const token = headers.authorization;
   const operation = body.operationName;
   try {
-    if (operation !== 'SignupUser') {
-      const { userId } = await jwt.verify(token, secret);
+    // Require auth token validation all operations, but bypass for SignupUser & LoginUser Mutations
+    if (operation !== 'SignupUser' && operation !== 'LoginUser') {
+      const { userId } = jwt.verify(token, secret);
       return { userId };
     }
   } catch (err) {
-    throw err;
+    throw new AuthenticationError('You must be logged in', 401); 
   }
 }
