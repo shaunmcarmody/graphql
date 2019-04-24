0c4f69caefd2fd239c4f12e234671f3d22d170ad
shaunmcarmody <shaun.carmody@live.co.uk>
2019-04-24 18:04:04 +0100

    remove signup helper

--- server/src/controllers/signup/signup.model.js
+++ server/src/controllers/signup/signup.model.js
@@ -1,11 +1,5 @@
 const db = require('../../data/dbConfig.js');
 
-const getUserById = id =>
-  db('users')
-    .where('id', id)
-    .select('id')
-    .first();
-
 const insertUser = user =>
   db('users')
     .insert(user)
